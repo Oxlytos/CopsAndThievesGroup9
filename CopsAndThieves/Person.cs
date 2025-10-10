1eb059d3c859cb0459b52331a25514760839649f
Oscar Stenström <oscar.stenstrom2001@gmail.com>
2025-10-10 11:31:31 +0200

    Inital project creation

---
+++ CopsAndThieves/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopsAndThieves
+{
+    internal class Person
+    {
+    }
+}
---
+++ CopsAndThieves/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CopsAndThieves
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
