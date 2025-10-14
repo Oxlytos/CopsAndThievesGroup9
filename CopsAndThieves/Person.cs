22515ed08e7e224bfe2a5692f7c3f019ddff7753
Joel Wahlström <Joelwahl85@gmail.com>
2025-10-14 18:12:50 +0200

    Förändringar i Person.cs

--- CopsAndThieves/Person.cs
+++ CopsAndThieves/Person.cs
@@ -8,6 +8,26 @@ namespace CopsAndThieves
 {
     internal class Person
     {
-
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public List<string> Inventory { get; set; }
+        public static string Sprite { get; set; }
     }
+
+    class Police : Person
+    {
+        //Sprite = "P";
+        void Arrest() { }
+    }
+
+    class Theif : Person
+    {
+        void Steal() { }
+    }
+
+    class Citizen : Person
+    {
+        void Greet() { }
+    }
+
 }
