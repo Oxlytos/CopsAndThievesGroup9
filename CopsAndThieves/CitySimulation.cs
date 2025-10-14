853bf479997282d54081db8db66ee7a7fb422a03
Oscar Stenström <oscar.stenstrom2001@gmail.com>
2025-10-14 10:24:03 +0200

    Draw basic city with paramters

--- CopsAndThieves/CitySimulation.cs
+++ CopsAndThieves/CitySimulation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,41 +9,53 @@ namespace CopsAndThieves
 {
     internal class CitySimulation
     {
+        static int width = 30;
+        static int height = 10;
 
+        int[,] citySize = { { 100 }, { 100 } };
 
-        int[,] citySize = { { 100 }, { 100 } }; 
-
-       public static void Run()
+        public static void Run()
         {
             Console.WriteLine("City");
 
-            for(int i = 0; i<10; i++)
+            string symbolForRoof = "=";
+            string symbolForPillar = "X";
+            //Draw along the X axis
+                Console.ForegroundColor = ConsoleColor.Green;
+            for(int z = 0; z <= width+1; z++)
             {
-               // bool writeHash = true;
-               //Roof
-                Console.Write(".");
-
-              
-                //Console.WriteLine();
+                Console.Write($"{symbolForRoof}");
             }
-            for (int j = 0; j < 10; j++)
+               
+
+                //Change line after loop is done
+
+                Console.WriteLine();
+                //Y Axis part that uses WriteLine
+                //Handles the height of a grid, left and right
+                for (int innerWall = 0; innerWall < height; innerWall++)
+                {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write($"{symbolForPillar}");
+                for (int space = 0; space < width; space++) 
+                {
+                    Console.Write(".");
+                }
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"{symbolForPillar}");
+                }
+
+                //Bottom part of the wholass graphic
+                //Like the roof, outside its loop in this case
+            for (int col = 0; col <= width+1; col++)
             {
-                Console.WriteLine(".");
+                
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write($"{symbolForRoof}");
 
-                //  writeHash = !writeHash;
             }
-            for (int i = 0; i < 10; i++)
-            {
-                // bool writeHash = true;
-                //Roof
-                Console.Write(".");
-
-
-                //Console.WriteLine();
-            }
-
+            Console.ForegroundColor = ConsoleColor.White;
 
         }
-
     }
 }
