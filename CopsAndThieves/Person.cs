ccf7c92aec57523927ecc3c1deba77a59c122225
TestOscar <testoscarstenstrom@gmail.com>
2025-10-14 13:23:16 +0200

    Testat ändringar från en annan av Oscars datorer

--- CopsAndThieves/Person.cs
+++ CopsAndThieves/Person.cs
@@ -8,6 +8,63 @@ namespace CopsAndThieves
 {
     internal class Person
     {
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+        public List<string> Inventory { get; set; }
+        public string Sprite { get; set; }
 
+        void Move()
+        {
+
+        }
     }
+
+    class Police : Person
+    {
+        public Police(string fName, string sName) 
+        {
+        
+            FirstName = fName;
+            SurName = sName;
+            Sprite = "P";
+        }
+
+
+        //Arrest a criminal
+        void Arrest() 
+        {
+        
+        }
+    }
+    
+    class Theif : Person
+    {
+
+        public Theif(string fName, string sName) 
+        {
+            FirstName = fName;
+            SurName = sName;
+            Sprite = "T";
+        }
+
+        //Steal from a citizen
+        void Steal() 
+        {
+        
+        }
+    }
+
+    class Citizen : Person
+    {
+        public Citizen(string fName, string sName) 
+        {
+            FirstName = fName;
+            SurName = sName;
+            Sprite = "C";
+        }
+
+        //Citizens greet either another citizen or a cop
+        void Greet() { }
+    }
+
 }
--- CopsAndThieves/Program.cs
+++ CopsAndThieves/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace CopsAndThieves
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopsAndThieves
 {
-    internal class Program
+    internal class Person
     {
-        static void Main(string[] args)
-        {
-            CitySimulation.Run();
-        }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public List<string> Inventory { get; set; }
+        public static string Sprite { get; set; }
     }
+
+    class Police : Person
+    {
+        //Sprite = "P";
+        void Arrest() { }
+    }
+
+    class Theif : Person
+    {
+        void Steal() { }
+    }
+
+    class Citizen : Person
+    {
+        void Greet() { }
+    }
+
 }
