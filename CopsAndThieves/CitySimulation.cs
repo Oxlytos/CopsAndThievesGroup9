509cf6ade5ad0394207e030535b694436d94bfba
Oscar Stenström <oscar.stenstrom2001@gmail.com>
2025-10-14 09:23:05 +0200

    La till city simulation.cs

---
+++ CopsAndThieves/CitySimulation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopsAndThieves
+{
+    internal class CitySimulation
+    {
+
+
+        int[,] citySize = { { 100 }, { 100 } }; 
+
+       public static void Run()
+        {
+            Console.WriteLine("City");
+
+            for(int i = 0; i<100; i++)
+            {
+                bool writeHash = true;
+               // Console.WriteLine("#");
+
+                for(int j = 0; j < 100; j++)
+                {
+                    if (writeHash)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write("$");
+                    }
+
+                    writeHash = !writeHash;
+                }
+                Console.WriteLine();
+            }
+        
+        
+        }
+
+    }
+}
